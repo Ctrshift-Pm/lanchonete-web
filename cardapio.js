28b5a502d81119c6dc088321ede5322785656181
Ctrshift-Pm <pedromatheus.ch@gmail.com>
2026-06-14 23:14:29 -0300

    feat: adiciona dados iniciais do cardapio

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,7 @@
+const itensCardapio = [
+  { nome: "X-Burguer", preco: 18.90, categoria: "Lanches" },
+  { nome: "X-Bacon", preco: 21.90, categoria: "Lanches" },
+  { nome: "Combo Familia", preco: 45.90, categoria: "Combos" }
+];
 
+console.log("Cardapio carregado:", itensCardapio);
