e99a7839de600299f2ae8ce6e2186d20425de46b
Ctrshift-Pm <pedromatheus.ch@gmail.com>
2026-06-14 23:16:19 -0300

    feat: expande dados do cardapio interativo

--- cardapio.js
+++ cardapio.js
@@ -1,7 +1,8 @@
 const itensCardapio = [
-  { nome: "X-Burguer", preco: 18.90, categoria: "Lanches" },
-  { nome: "X-Bacon", preco: 21.90, categoria: "Lanches" },
-  { nome: "Combo Familia", preco: 45.90, categoria: "Combos" }
+  { nome: "X-Burguer", preco: 18.90, categoria: "Lanches", destaque: false },
+  { nome: "X-Bacon", preco: 21.90, categoria: "Lanches", destaque: true },
+  { nome: "Combo Familia", preco: 45.90, categoria: "Combos", destaque: true },
+  { nome: "Milkshake 500ml", preco: 10.90, categoria: "Bebidas", destaque: false }
 ];
 
 console.log("Cardapio carregado:", itensCardapio);
